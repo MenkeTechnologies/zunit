6aa0e20d1c4d31cbcbad15ce135ce6e9b8f10199
MenkeTechnologies <linux.dev25@gmail.com>
2020-09-06 05:23:07 -0400

    missing dir

--- zunit.plugin.zsh
+++ zunit.plugin.zsh
@@ -1,36 +1,38 @@
 0="${${0:#$ZSH_ARGZERO}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-plugin_dir="${0:h}"
+zunit_plugin_dir="${0:h}"
 
-if [[ ! -f $plugin_dir/bin/zunit ]]; then
+if [[ ! -f $zunit_plugin_dir/bin/zunit ]]; then
     # Clear the file to start with
     if [[ ! -d bin ]]; then
-        command mkdir $plugin_dir/bin \
+        command mkdir $zunit_plugin_dir/bin \
         > /dev/null 2>&1
     fi
-    cat /dev/null > $plugin_dir/bin/zunit
+    cat /dev/null > $zunit_plugin_dir/bin/zunit
 
     # Start with the shebang
-    echo "#!/usr/bin/env zsh\n" >> $plugin_dir/bin/zunit
+    echo "#!/usr/bin/env zsh\n" >> $zunit_plugin_dir/bin/zunit
 
     # We need to do some fancy globbing
     setopt EXTENDED_GLOB
 
     # Print each of the source files into the target, removing any comments
     # and blank lines from the compiled executable
-    cat $plugin_dir/src/**/(^zunit).zsh | grep -v -E '^(\s*#.*[^"]|\s*)$' >> $plugin_dir/bin/zunit
+    cat $zunit_plugin_dir/src/**/(^zunit).zsh | grep -v -E '^(\s*#.*[^"]|\s*)$' >> $zunit_plugin_dir/bin/zunit
 
     # Print the main command last
-    cat $plugin_dir/src/zunit.zsh | grep -v -E '^(\s*#.*[^"]|\s*)$' >> bin/zunit
+    cat $zunit_plugin_dir/src/zunit.zsh | grep -v -E '^(\s*#.*[^"]|\s*)$' >> $zunit_plugin_dir/bin/zunit
 
     # Make sure the file is executable
-    chmod u+x $plugin_dir/bin/zunit
+    chmod u+x $zunit_plugin_dir/bin/zunit
 
     # Let the user know we're finished
     echo "\033[0;32m✔\033[0;m ZUnit built successfully"
 
 fi
+
+unset zunit_plugin_dir
 
 path=("${0:h}/bin" $path)
 fpath=("${0:h}/src_comp" $fpath)
